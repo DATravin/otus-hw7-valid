586a912692f6befb2783a63618b9af8356ad9d1f
Травин Дмитрий Андреевич <DTravin@vimpelcom.ru>
2025-01-28 11:57:20 +0300

    fix: test

--- src/model_fit_2.py
+++ src/model_fit_2.py
@@ -308,20 +308,20 @@ def main():
 
     X_train = pipeline_preprocess.fit(train_sdf).transform(train_sdf)
 
-#     rf = RandomForestClassifier()\
-#         .setFeaturesCol('scaledFeatures')\
-#         .setLabelCol('target')\
-#         .setMaxDepth(best_params['maxDepth'])\
-#         .setNumTrees(best_params['numTrees'])\
+    rf = RandomForestClassifier()\
+        .setFeaturesCol('scaledFeatures')\
+        .setLabelCol('target')\
+        .setMaxDepth(best_params['maxDepth'])\
+        .setNumTrees(best_params['numTrees'])\
 
-#     rf.fit(X_train)
+    rf.fit(X_train)
 
-#     X_test = pipeline_preprocess.transform(test_sdf)
+    X_test = pipeline_preprocess.transform(test_sdf)
 
-#     evaluator = BinaryClassificationEvaluator()\
-#             .setLabelCol('target')
+    evaluator = BinaryClassificationEvaluator()\
+            .setLabelCol('target')
 
-#     auc_final = evaluator.evaluate(rf.transform(X_test))
+    auc_final = evaluator.evaluate(rf.transform(X_test))
 
 #     model_name = 'classification'
 
